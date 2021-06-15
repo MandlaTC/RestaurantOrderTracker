be2a2dfa2e6bc3dd09f7b732d4cf790df3810e92
taufeeq849 <taufeeq849@gmail.com>
2021-06-15 16:50:47 +0200

    added customer bottom nav bar

--- app/src/main/java/com/example/view/home/CustomerHomeActivity.java
+++ app/src/main/java/com/example/view/home/CustomerHomeActivity.java
@@ -1,25 +1,39 @@
 package com.example.view.home;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.customer_fragments.PastOrdersFragment;
+import com.example.customer_fragments.UpcomingOrdersFragment;
+import com.example.data_models.User;
 import com.example.model.AuthRepository;
 import com.example.testrequests.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class CustomerHomeActivity extends AppCompatActivity {
-
+public class CustomerHomeActivity extends FragmentActivity {
     TextView logOutButton;
     Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.customer_home_layout);
+        setContentView(R.layout.customer_bottom_navigation_view);
         context = this;
+        getUser();
+        initBottomNavBar();
+
+    }
+
+    public void setLogOutButton() {
         logOutButton = findViewById(R.id.customer_logout_button);
         logOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,4 +42,50 @@ public class CustomerHomeActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    public void initBottomNavBar() {
+
+        BottomNavigationView bottomNav = findViewById(R.id.customer_bottom_nav);
+
+        openFragment(new PastOrdersFragment());
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.customer_navigation_past_orders:
+                        openFragment(new PastOrdersFragment());
+                        return true;
+
+                    case R.id.customer_navigation_upcoming_orders:
+                        openFragment(new UpcomingOrdersFragment());
+                        return true;
+
+                }
+
+                return false;
+            }
+        });
+
+
+    }
+
+    public void getUser() {
+        User user = AuthRepository.getSavedUserFromPreference(context);
+        if (user != null) {
+            System.out.println(user.toString());
+        }
+    }
+
+    void openFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.customer_frame_layout, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+    }
 }
+
